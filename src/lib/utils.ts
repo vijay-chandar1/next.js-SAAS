59d0460aa228503005335d607ecc33b194734207
vijay-chandar1 <167110584+vijay-chandar1@users.noreply.github.com>
2024-08-08 08:18:54 +0000

    Initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,8 +16,8 @@ export function absoluteUrl(path: string) {
 }
 
 export function constructMetadata({
-  title = "Quill - the SaaS for students",
-  description = "Quill is an open-source software to make chatting to your PDF files easy.",
+  title = "Samyak Tech Labs OCR",
+  description = "Software designed to make OCR in PDF easy.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
   noIndex = false
